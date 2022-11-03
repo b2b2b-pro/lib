5e78d0255bb1363dd9af8ea972bbd7e6d82ceeac
maris-cyber <maris.sabirov@gmail.com>
2022-11-03 16:03:28 +0700

    дописал функцию listObligation для тестирования авторизации

--- repo_client/obligation_req.go
+++ repo_client/obligation_req.go
@@ -1,6 +1,14 @@
 package repo_client
 
-import "github.com/b2b2b-pro/lib/object"
+import (
+	"context"
+	"io"
+	"time"
+
+	"github.com/b2b2b-pro/lib/object"
+	"github.com/b2b2b-pro/lib/torepo"
+	"go.uber.org/zap"
+)
 
 func (gc *RepoGRPC) CreateObligation(tkn string, obl object.Obligation) (int, error) {
 	return 1, nil
@@ -8,5 +16,30 @@ func (gc *RepoGRPC) CreateObligation(tkn string, obl object.Obligation) (int, er
 
 func (gc *RepoGRPC) ListObligation(tkn string) ([]object.Obligation, error) {
 	r := []object.Obligation{}
-	return r, nil
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	stream, err := gc.client.ListObligation(ctx, &torepo.Tkn{Tkn: tkn})
+	if err != nil {
+		zap.S().Debugf("ListEntity error: %v\n", err)
+		return nil, err
+	}
+
+	for {
+		x, err := stream.Recv()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			zap.S().Debugf("Loop ListObligation error: %v\n", err)
+		}
+
+		r = append(r, *object.MObligation(x))
+		zap.S().Debugf("Loop stream recv: %v\n", x.Id)
+	}
+
+	return r, err
 }
